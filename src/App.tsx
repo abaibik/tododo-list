9ddbfa4053f8f5b5797025d04ec8c4a4f9fd7390
Aleksandra Baibik <abaibik@gmail.com>
2023-04-15 18:33:26 +0200

    center logo and heading

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import { List } from "./List";
 import logo from "./images/icons8-test-100.png";
-import { Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Container className="m-5">
         <div className="heading-with-logo">
-          <h1>Tododo-list</h1>
-
           <img alt="Logo Todo-list" className="logo" src={logo} />
+          <div className="align-self-center">
+            <h1>Tododo-list</h1>
+          </div>
         </div>
 
         <List />
