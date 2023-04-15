4fbb194813238a9f60efceb157a2fe0c4900d92c
Aleksandra Baibik <abaibik@gmail.com>
2023-04-15 20:27:44 +0200

    checked items marked and overscored

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, ListGroup } from "react-bootstrap";
 
 type ListItemProps = {
@@ -6,9 +6,23 @@ type ListItemProps = {
 };
 
 export const ListItem: React.FC<ListItemProps> = ({ label }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  const className = checked ? "checked-item" : "";
+
   return (
     <ListGroup.Item>
-      <Form.Check inline label={label} />
+      <Form.Check
+        checked={checked}
+        onChange={handleChange}
+        inline
+        label={label}
+        className={className}
+      />
     </ListGroup.Item>
   );
 };
