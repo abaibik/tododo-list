d1ba80ed7c55eeba25bdab7680ae7339f608cc2a
Aleksandra Baibik <abaibik@gmail.com>
2023-04-15 18:26:06 +0200

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import { List } from "./List";
+import logo from "./images/icons8-test-100.png";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Container className="m-5">
-        <h1>Tododo-list</h1>
+        <div className="heading-with-logo">
+          <h1>Tododo-list</h1>
+
+          <img alt="Logo Todo-list" className="logo" src={logo} />
+        </div>
+
         <List />
       </Container>
     </div>
