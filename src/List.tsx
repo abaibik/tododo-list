e9148fc97273c5a491857a71c20627d219ad6d67
Aleksandra Baibik <abaibik@gmail.com>
2023-04-16 14:37:37 +0200

    add uuid, 2 lists for checked and unchecked items

--- src/List.tsx
+++ src/List.tsx
@@ -3,10 +3,12 @@ import { Row, Col, Button, ListGroup } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import { ModalAddItem } from "./ModalAddItem";
 import { ListItem } from "./ListItem";
+import { v4 as uuidv4 } from "uuid";
 
 type ToDoItem = {
   text: string;
   done: boolean;
+  id: string;
 };
 
 export function List() {
@@ -15,18 +17,24 @@ export function List() {
   const handleShow = () => setShowModal(true);
 
   const [items, setItems] = useState<ToDoItem[]>([
-    { text: "Buy some food", done: false },
-    { text: "Feed cats", done: false },
+    { text: "Buy some food", done: false, id: uuidv4() },
+    { text: "Feed cats", done: false, id: uuidv4() },
   ]);
 
   const handleAdd = (text: string) => {
-    setItems([...items, { text, done: false }]);
+    setItems([...items, { text, done: false, id: uuidv4() }]);
     handleClose();
   };
 
-  const handleChange = (checked: boolean, idx: number) => {
-    const currentItem = items[idx];
-    const itemsWithoutCurrent = items.filter((item, index) => index !== idx);
+  const uncheckedItems = items.filter((item) => !item.done);
+  const checkedItems = items.filter((item) => item.done);
+
+  const handleChange = (checked: boolean, id: string) => {
+    const currentItemIdx = items.findIndex((el) => el.id === id);
+    const currentItem = items[currentItemIdx];
+    const itemsWithoutCurrent = items.filter(
+      (item, index) => index !== currentItemIdx
+    );
     setItems([...itemsWithoutCurrent, { ...currentItem, done: checked }]);
   };
 
@@ -40,13 +48,24 @@ export function List() {
       <Row>
         <Col>
           <ListGroup>
-            {items.map((item, idx) => (
+            {uncheckedItems.map((item) => (
               <ListItem
                 key={item.text}
                 label={item.text}
                 checked={item.done}
                 onChange={(checked) => {
-                  handleChange(checked, idx);
+                  handleChange(checked, item.id);
+                }}
+              />
+            ))}
+
+            {checkedItems.map((item) => (
+              <ListItem
+                key={item.text}
+                label={item.text}
+                checked={item.done}
+                onChange={(checked) => {
+                  handleChange(checked, item.id);
                 }}
               />
             ))}
