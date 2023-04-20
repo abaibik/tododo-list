9afa71fd9dadaf74313b8d57873ddbcf157ce261
Aleksandra Baibik <abaibik@gmail.com>
2023-04-20 18:08:15 +0200

    disable testing-library/no-node-access for tests

--- src/ListItem.test.tsx
+++ src/ListItem.test.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable testing-library/no-node-access */
 import React from "react";
 import { render } from "@testing-library/react";
 import { ListItem } from "./ListItem";
 
-test("ListItem has another style when done", () => {
+test("ListItem schould have another style when done", () => {
   const { container } = render(
     <ListItem label="cook lunch" checked={true} onChange={() => {}} id="1" />
   );
